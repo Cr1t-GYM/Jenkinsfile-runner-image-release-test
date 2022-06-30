d9a0baf3be148c4dbaba3aa835dbf071e6add7f4
Yiming Gong <277645526@qq.com>
2022-06-30 16:45:33 -0400

    reuse previous step container

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ rm -rf tmp .build source out jenkinsfile-runner
 ARTIFACT_ID=jenkinsfile-runner-demo
 VERSION=256.0-test
 CWP_VERSION=1.3
-JENKINSFILE_RUNNER_TAG=jenkins-local/jenkinsfile-runner-single
 
 git clone https://github.com/jenkinsci/jenkinsfile-runner
 rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/target/*' jenkinsfile-runner/* source
@@ -14,5 +13,3 @@ rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/targe
 mkdir -p .build
 wget -O .build/cwp-cli-${CWP_VERSION}.jar https://repo.jenkins-ci.org/releases/io/jenkins/tools/custom-war-packager/custom-war-packager-cli/${CWP_VERSION}/custom-war-packager-cli-${CWP_VERSION}-jar-with-dependencies.jar
 java -jar .build/cwp-cli-${CWP_VERSION}.jar -configPath packager-config.yml -tmpDir out/tmp -version ${VERSION}
-
-docker build -t ${JENKINSFILE_RUNNER_TAG} -f out/tmp/output/Dockerfile out/tmp/output/
