69be67fb74c8ae1a988e9f1deaca2dfb8b34a795
Yiming Gong <277645526@qq.com>
2022-06-30 15:32:15 -0400

    debug the second phase

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf tmp .build source out jenkinsfile-runner
+# rm -rf tmp .build source out jenkinsfile-runner
 
 ARTIFACT_ID=jenkinsfile-runner-demo
 VERSION=256.0-test
 CWP_VERSION=1.3
 JENKINSFILE_RUNNER_TAG=jenkins-local/jenkinsfile-runner-single
+JENKINS_ACTION_TAG=jenkins-local/jenkinsfile-action
 
-git clone https://github.com/jenkinsci/jenkinsfile-runner
-rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/target/*' jenkinsfile-runner/* source
+# git clone https://github.com/jenkinsci/jenkinsfile-runner
+# rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/target/*' jenkinsfile-runner/* source
 
-mkdir -p .build
-wget -O .build/cwp-cli-${CWP_VERSION}.jar https://repo.jenkins-ci.org/releases/io/jenkins/tools/custom-war-packager/custom-war-packager-cli/${CWP_VERSION}/custom-war-packager-cli-${CWP_VERSION}-jar-with-dependencies.jar
-java -jar .build/cwp-cli-${CWP_VERSION}.jar -configPath packager-config.yml -tmpDir out/tmp -version ${VERSION}
+# mkdir -p .build
+# wget -O .build/cwp-cli-${CWP_VERSION}.jar https://repo.jenkins-ci.org/releases/io/jenkins/tools/custom-war-packager/custom-war-packager-cli/${CWP_VERSION}/custom-war-packager-cli-${CWP_VERSION}-jar-with-dependencies.jar
+# java -jar .build/cwp-cli-${CWP_VERSION}.jar -configPath packager-config.yml -tmpDir out/tmp -version ${VERSION}
 
-# docker build -t ${JENKINSFILE_RUNNER_TAG} -f $(shell pwd)/out/tmp/output/Dockerfile $(shell pwd)/out/tmp/output/	
+docker build -t ${JENKINSFILE_RUNNER_TAG} -f out/tmp/output/Dockerfile out/tmp/output/
+docker build -t ${JENKINS_ACTION_TAG} -f Dockerfile .	
