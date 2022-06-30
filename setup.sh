f927954b00d6d93ee5d45a38d29b02de5a9b5791
Yiming Gong <277645526@qq.com>
2022-06-30 15:21:13 -0400

    fix syntax

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+rm -rf tmp .build source out
+
 ARTIFACT_ID=jenkinsfile-runner-demo
 VERSION=256.0-test
 CWP_VERSION=1.3
 JENKINSFILE_RUNNER_TAG=jenkins-local/jenkinsfile-runner-single
 
 git clone https://github.com/jenkinsci/jenkinsfile-runner
-rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/target/*' jenkinsfile-runner source
+rsync -av --exclude='.git' --exclude='demo' --exclude='tests' --exclude='*/target/*' jenkinsfile-runner/* source
 
 mkdir -p .build
 wget -O .build/cwp-cli-${CWP_VERSION}.jar https://repo.jenkins-ci.org/releases/io/jenkins/tools/custom-war-packager/custom-war-packager-cli/${CWP_VERSION}/custom-war-packager-cli-${CWP_VERSION}-jar-with-dependencies.jar
