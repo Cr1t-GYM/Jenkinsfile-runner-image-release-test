224a5fec0e956185c161cc69d0503bd4d4214f5e
Yiming Gong <277645526@qq.com>
2022-07-11 12:18:10 -0400

    test in the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo "$ACTIONS_CACHE_URL $GITHUB_REF"
-git clone https://github.com/actions/cache
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 node -v
 INPUT_KEY="test" INPUT_PATH="tmp" ACTIONS_CACHE_URL=$ACTIONS_CACHE_URL node cache/dist/restore/index.js
