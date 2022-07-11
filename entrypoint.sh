393dce63285e8d445a5aec4efe33092615bc3418
Yiming Gong <277645526@qq.com>
2022-07-11 12:22:07 -0400

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 echo "$ACTIONS_CACHE_URL $GITHUB_REF"
-curl -fsSL https://deb.nodesource.com/setup_18.x | -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x > node_setup.sh
+chmod +x node_setup.sh
+./node_setup.sh
 node -v
 INPUT_KEY="test" INPUT_PATH="tmp" ACTIONS_CACHE_URL=$ACTIONS_CACHE_URL node cache/dist/restore/index.js
